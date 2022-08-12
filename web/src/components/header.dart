99a590bf2c01867455f29395e3500782f2f01280
tangshuai <tangshuai@wisecotech.com>
2022-08-12 16:12:56 +0800

    feat: menu init

--- web/src/components/header.dart
+++ web/src/components/header.dart
@@ -1,7 +1,49 @@
 import 'dart:html';
 
+DivElement getLogo() {
+  DivElement logo = DivElement();
+  logo.className = 'logo';
+
+  HeadingElement h1 = HeadingElement.h1();
+  h1.text = 'Mole';
+
+  logo.children.add(h1);
+  return logo;
+}
+
+UListElement getMenu() {
+  UListElement menuList = UListElement();
+  var menuArray = [
+    {
+      'name': 'Home',
+      'link': '/home',
+    },
+    {
+      'name': 'Blog',
+      'link': '/blog',
+    },
+    {
+      'name': 'Market',
+      'link': '/market',
+    },
+  ];
+  for (final menu in menuArray) {
+    LIElement menuLiList = LIElement();
+    AnchorElement link = AnchorElement();
+    link.href = menu['link'];
+    link.text = menu['name'];
+    menuLiList.children.add(link);
+    menuList.children.add(menuLiList);
+  }
+  return menuList;
+}
+
 DivElement getHeader() {
   DivElement headerContainer = DivElement();
-  headerContainer.text = 'test';
+  headerContainer.className = 'header-container';
+
+  headerContainer.children.add(getLogo());
+  headerContainer.children.add(getMenu());
+
   return headerContainer;
 }
